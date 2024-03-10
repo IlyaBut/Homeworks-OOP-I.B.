74a8c5c4314718135b5079328e7547ffb0a5fb6b
Ilya Butenko <Forgithubtyy@gmail.com>
2024-03-10 17:25:10 +0300

    Add lecturer reviwer

--- main.py
+++ main.py
@@ -7,24 +7,40 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def __str__(self):
+        return f"Имя:{self.name}\nФамилия:{self.surname}\nСредняя оценка за домашнее задание:{}\nКурсы в процессе изучения:{self.courses_in_progress}\nЗавершенные курсы:{self.finished_courses}"
+
+    def rate_Lecturer(self, Lecturer, course, grade):
+        if isinstance(Lecturer, Student) and course in self.courses_attached and course in Lecturer.courses_in_progress:
+            if course in Lecturer.grades:
+                Lecturer.grades[course] += [grade]
+            else:
+                Lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+    def __str__(self):
+        return f"Имя{self.name}\nФамилия{self.surname}\nСредняя оценка за лекции:{}"
+
+
 class Reviewer(Mentor):
-    pass
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+    def __str__(self):
+        return f"Имя{self.name}\nФамилия{self.surname}"
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
